ed343d228f624fdcb33286018338d9f58f2a73ec
sravel <sebastien.ravel@cirad.fr>
2016-05-20 14:37:44 +0200

    add commit and cp to NAS_BGPI

--- epydocUpdate.sh
+++ epydocUpdate.sh
@@ -10,5 +10,6 @@ epydoc --html ./modules/MODULES_SEB.py -o ./modules/DOC/ -v
 
 mv ./index.html ./modules/DOC/
 
+git commit -m "update DOC for MODULE_SEB" ./modules/DOC/*
 
-scp DOC/* admin@194.254.138.95:/volume1/web/DOC/
+scp ./modules/DOC/* admin@194.254.138.95:/volume1/web/DOC/
