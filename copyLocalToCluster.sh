f8171f5f3c93b638e1b1ef033c6593908a63e9e1
sravel <sebastien.ravel@cirad.fr>
2016-05-20 10:18:42 +0200

    build script to sed shebang

--- copyLocalToCluster.sh
+++ copyLocalToCluster.sh
@@ -1,19 +1,23 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+## @package copyLocalToCluster.sh
+# @author Sebastien Ravel
 
 
 
+cp ./local/* ./cluster/
 
 
+for f in ./cluster/*.py
+do
+
+	sed -i -e "s|#!/usr/bin/python3.*|#!/usr/local/bioinfo/python/3.4.3_build2/bin/python|" $f
+	sed -i -e "s|#!/usr/bin/python2.*|#!/usr/local/bioinfo/python/2.7.9_build2/bin/python|" $f
+done
 
 
+for f in ./cluster/*.R
+do
 
-#!/usr/bin/python3.5
-#!/usr/local/bioinfo/python/3.4.3_build2/bin/python
-
-
-
-#!/usr/bin/Rscript --vanilla
-#!/usr/local/bioinfo/R/3.2.2/bin/Rscript --vanilla
-
-
-#!/usr/bin/python2.7
-#!/usr/local/bioinfo/python/2.7.9_build2/bin/python
+	sed -i -e "s|#!/usr/bin/Rscript --vanilla|#!/usr/local/bioinfo/R/3.2.2/bin/Rscript --vanilla|" $f
+done
