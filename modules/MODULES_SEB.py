5e053188c7a7dc274b9625fb97269e05e4f929bd
sravel <sebastien.ravel@cirad.fr>
2016-07-08 10:38:16 +0200

    add attrubut len and seq to GFFRecord()

--- modules/MODULES_SEB.py
+++ modules/MODULES_SEB.py
@@ -905,11 +905,12 @@ def relativeToAbsolutePath(relative):
 #################################################
 
 class parseGFF():
+	"""["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes", "seq", "len"]"""
 
 	def __init__(self, filename):
 		#Initialized GeneInfo named tuple. Note: namedtuple is immutable
 		self.filename = filename
-		self.gffInfoFields = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
+		self.gffInfoFields = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes", "seq", "len"]
 		self.GFFRecord = namedtuple("GFFRecord", self.gffInfoFields)
 
 	def parseGFFAttributes(self, attributeString):
@@ -918,7 +919,7 @@ class parseGFF():
 		ret = {}
 		for attribute in attributeString.split(";"):
 			key, value = attribute.split("=")
-			ret[urllib.unquote(key)] = urllib.unquote(value)
+			ret[urllib.parse.unquote(key)] = urllib.parse.unquote(value)
 		return ret
 
 	def parseGFF3(self):
@@ -935,18 +936,21 @@ class parseGFF():
 				if line.startswith("#"): continue
 				parts = line.strip().split("\t")
 				#If this fails, the file format is not standard-compatible
-				assert len(parts) == len(self.gffInfoFields)
+				assert len(parts) == len(self.gffInfoFields)-2
 				#Normalize data
 				normalizedInfo = {
-					"seqid": None if parts[0] == "." else urllib.unquote(parts[0]),
-					"source": None if parts[1] == "." else urllib.unquote(parts[1]),
-					"type": None if parts[2] == "." else urllib.unquote(parts[2]),
+					"seqid": None if parts[0] == "." else urllib.parse.unquote(parts[0]),
+					"source": None if parts[1] == "." else urllib.parse.unquote(parts[1]),
+					"type": None if parts[2] == "." else urllib.parse.unquote(parts[2]),
 					"start": None if parts[3] == "." else int(parts[3]),
 					"end": None if parts[4] == "." else int(parts[4]),
+					"len": None if parts[4] == "."  and parts[3] == "." else int(parts[4])-int(parts[3]),
 					"score": None if parts[5] == "." else float(parts[5]),
-					"strand": None if parts[6] == "." else urllib.unquote(parts[6]),
-					"phase": None if parts[7] == "." else urllib.unquote(parts[7]),
+					"strand": None if parts[6] == "." else urllib.parse.unquote(parts[6]),
+					"phase": None if parts[7] == "." else urllib.parse.unquote(parts[7]),
+					"seq": None,
 					"attributes": self.parseGFFAttributes(parts[8])
+
 				}
 				#Alternatively, you can emit the dictionary here, if you need mutability:
 				#	yield normalizedInfo
