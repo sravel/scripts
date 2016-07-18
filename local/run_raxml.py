bdbc55aa0a547ef2f9f57f5fbbd98ece3d420900
sravel <sebastien.ravel@cirad.fr>
2016-07-18 16:33:11 +0200

    end scritp

--- local/run_raxml.py
+++ local/run_raxml.py
@@ -11,7 +11,6 @@ import sys, os, shutil
 current_dir = os.path.dirname(os.path.abspath(__file__))+"/"
 sys.path.insert(1,current_dir+'../modules/')
 from MODULES_SEB import relativeToAbsolutePath, existant_file, directory, printCol
-
 ## Python modules
 import argparse
 from time import localtime, strftime
