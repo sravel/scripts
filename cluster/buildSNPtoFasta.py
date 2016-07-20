104acefd159059d87f67817fcc31485d9620b752
sravel <sebastien.ravel@cirad.fr>
2016-07-20 09:33:59 +0200

    update Cluster scripts

--- cluster/buildSNPtoFasta.py
+++ cluster/buildSNPtoFasta.py
@@ -10,7 +10,7 @@
 import sys, os
 current_dir = os.path.dirname(os.path.abspath(__file__))+"/"
 sys.path.insert(1,current_dir+'../modules/')
-from MODULES_SEB import relativeToAbsolutePath, existant_file, fasta2dict, directory, parseGFF
+from MODULES_SEB import relativeToAbsolutePath, existant_file, fasta2dict, directory, parseGFF, loadInList, dict2txt, AutoVivification, dictDict2txt
 
 ## Python modules
 import argparse
@@ -18,7 +18,8 @@ from time import localtime, strftime
 
 ## BIO Python modules
 from Bio import SeqIO
-
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 
 ##################################################
 ## Variables Globales
@@ -47,38 +48,169 @@ if __name__ == "__main__":
 
 	files = parser.add_argument_group('Input info for running')
 	files.add_argument('-g', '--gff', metavar="<filename>", type=existant_file, required=True, dest = 'gffFile', help = 'gff files with annotation')
-	#files.add_argument('-f', '--fasta', metavar="<filename>", type=existant_file, required=True, dest = 'fastaFile', help = 'fasta files to split')
-	#files.add_argument('-o', '--out', metavar="<path/to/directory>", type = directory, required=True, dest = 'pathOut', help = 'Name of output file directory')
+	files.add_argument('-l', '--list', metavar="<filename>", type=existant_file, required=True, dest = 'listKeepFile', help = 'File with geneID to keep')
+	files.add_argument('-t', '--tab', metavar="<filename>", type=existant_file, required=True, dest = 'tabFile', help = 'File with SNP')
+	files.add_argument('-f', '--fasta', metavar="<path/to/directory>", type = directory, required=True, dest = 'fastaPath', help = 'Directory with fasta of 3 strains')
+	files.add_argument('-o', '--out', metavar="<path/to/directory>", type = directory, required=True, dest = 'pathOut', help = 'Name of output file directory')
 
 	# Check parameters
 	args = parser.parse_args()
 
 	#Welcome message
 	print("#################################################################")
-	print("#        Welcome in buildSNPtoFasta (Version " + version + ")          #")
+	print("#          Welcome in buildSNPtoFasta (Version " + version + ")          #")
 	print("#################################################################")
 	print('Start time: ', start_time,'\n')
 
-	# Récupère le fichier de conf passer en argument
+	# Récupère les arguments
 	gffFile = relativeToAbsolutePath(args.gffFile)
+	listKeepFile = relativeToAbsolutePath(args.listKeepFile)
+	tabFile = relativeToAbsolutePath(args.tabFile)
+	fastaPath = args.fastaPath
+	pathFileOut = args.pathOut
+
+
+
 	#fastaFile = relativeToAbsolutePath(args.fastaFile)
-	#pathFileOut = args.pathOut
 
 	print("\t - Input GFF is: %s" % gffFile)
-	#print("\t - Input Path is: %s" % pathFileOut.pathDirectory)
-	#print("\t - fasta file is : %s" % fastaFile)
+	print("\t - Input listKeppFile is: %s" % listKeepFile)
+	print("\t - Input tabFile is: %s" % tabFile)
+	print("\t - Input fasta files is: %s" % fastaPath.pathDirectory)
+	print("\t - Output fasta files is: %s" % pathFileOut.pathDirectory)
+
+	listKeepID = [ID.replace("Mycfi_gene","gene_") for ID in loadInList(listKeepFile)]
+	print("\nTotal ID Keep: %d" % len(listKeepID))
 
 
 	objGFF = parseGFF(gffFile)
 	recordCount = 0
-
+	dicoGenesKeepPosOnScaff = {}
+	keepValidList = []
 	for record in objGFF.parseGFF3():
-		#Apply filter, if any
-		#if args.filter_type and record.type != args.filter_type:
-			#continue
-		#Print record if specified by the user
-		if record.type == "CDS":
-			print(record)
-		#Access attributes like this: my_strand = record.strand
-		recordCount += 1
-	print("Total records: %d" % recordCount)
+
+		if record.type == "mRNA" :
+			transcriptID = record.attributes["transcriptId"]
+			geneID = "gene_"+record.attributes["transcriptId"]
+
+		if record.type == "CDS" and geneID in listKeepID :
+
+			if geneID not in keepValidList:
+				keepValidList.append(geneID)
+				recordCount += 1
+			for posIter in range(record.start, record.end):
+				if record.seqid not in dicoGenesKeepPosOnScaff.keys():
+					dicoGenesKeepPosOnScaff[record.seqid] = {posIter:geneID}
+				else:
+					dicoGenesKeepPosOnScaff[record.seqid].update({posIter:geneID})
+
+	print("\nTotal records: %d" % recordCount)
+
+	#print(dict2txt(dicoGenesKeepPosOnScaff))
+
+
+
+	# Parcours du fichier de SNP
+	nblignetotal,ctr = 74215263,0
+	dicoSeqBuild = {}
+	with open(tabFile, "r") as tabFileRead:
+		header = tabFileRead.readline().rstrip().split("\t")
+		soucheIndice = header[3:]
+		#print(header)
+
+		for line in tabFileRead:
+
+			if ((ctr % 10000 == 0) and (ctr != 0)) or (float(ctr) == nblignetotal):
+				percent = (float(ctr)/float(nblignetotal))*100
+				sys.stdout.write("\rProcessed up to %0.2f %%..." % percent)
+				sys.stdout.flush()
+
+			tabLine = line.rstrip().split("\t")
+			scaff = tabLine[0]
+			position = int(tabLine[1])
+			ref = tabLine[2]
+			if scaff in dicoGenesKeepPosOnScaff.keys() and position in dicoGenesKeepPosOnScaff[scaff].keys():
+				geneID = dicoGenesKeepPosOnScaff[scaff][position]
+				if geneID not in dicoSeqBuild.keys():
+					dicoSeqBuild[geneID] = {}
+
+
+				correctSNP = [snp.replace("R",ref).replace("U","N").replace("F","N") for snp in tabLine[3:]]
+				i=0
+				for SNP in correctSNP:
+					souche = soucheIndice[i]
+					#print(souche)
+					if souche not in dicoSeqBuild[geneID].keys():
+						dicoSeqBuild[geneID][souche] = SNP
+					else:
+						dicoSeqBuild[geneID][souche] += SNP
+					i+=1
+
+			ctr+=1
+
+
+	#print(dict2txt(dicoSeqBuild))
+
+	# chargement des sequences 3 souches
+	for files in fastaPath.lsExtInDirToList("fasta"):
+		geneID = files.split("_")[1].replace("gene","gene_")
+		dico = fasta2dict(files)
+		dicoSeqBuild[geneID].update(dico)
+
+
+	countOnlyN=0
+	listSeqNFind = []
+	for geneID, dico in dicoSeqBuild.items():
+		with open("/work/carlier.j/globalPopGenomicMF/buildSeq62/test/"+geneID+".fasta", "w") as output_handle:
+			seqNfind = False
+			for souche, txtseq in dico.items():
+
+				if type(txtseq) is not SeqRecord:
+					txtseqNdel = txtseq.replace("N","")
+					if len(txtseqNdel) == 0:
+						countOnlyN += 1
+						seqNfind=True
+					record = SeqRecord(Seq(txtseq),id=souche,name=souche, description="MicFi_"+geneID)
+					SeqIO.write(record,output_handle, "fasta")
+
+
+				else :
+					record = SeqRecord(txtseq.seq,id=souche,name=souche, description="MicFi_"+geneID)
+					SeqIO.write(record,output_handle, "fasta")
+
+
+			if seqNfind == True:
+				listSeqNFind.append(geneID)
+
+
+	print("\nNB file with seq Only N: %s" % len(listSeqNFind))
+	with open("listToRMgene.txt", "w") as toRMFile:
+		txt = "\n".join(listSeqNFind)
+		toRMFile.write(txt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	#print("\n\nExecution summary:")
+
+	#print("  - Outputting \n\
+	#Il y a au final %i Sequences gardées sur les %i initial\n\
+	#les sequences sont ajoutées dans le fichier %s" %(nbKeep,nbTotal,outputfilename))
+	#print("\nStop time: ", strftime("%d-%m-%Y_%H:%M:%S", localtime()))
+	print("\n\n#################################################################")
+	print("#                        End of execution                       #")
+	print("#################################################################")
