38c2248cc2b164ee232074a3d0c331c6be83ef5d
sravel <sebastien.ravel@cirad.fr>
2016-07-21 13:21:03 +0200

    update doc

--- local/extractSeqFastaFromLen.py
+++ local/extractSeqFastaFromLen.py
@@ -1,7 +1,50 @@
 #!/usr/bin/python3.5
 # -*- coding: utf-8 -*-
-## @package extractSeqFastaFromLen.py
+# @package extractSeqFastaFromLen.py
 # @author Sebastien Ravel
+
+"""
+	The extractSeqFastaFromLen script
+	===================================
+	:author: Sebastien Ravel
+	:contact: sebastien.ravel@cirad.fr
+	:date: 08/07/2016
+	:version: 0.1
+
+	Script description
+	------------------
+
+	This Programme filter sequences by length
+
+	Example
+	-------
+
+	>>> # Keep sequences greater than 1000
+	>>> extractSeqFastaFromLen.py -f sequences.fasta -l 1000 -o sequence_Sup1000.fasta -k g
+	>>> # Keep sequences lower than 1000
+	>>> extractSeqFastaFromLen.py -f sequences.fasta -l 1000 -o sequence_Inf1000.fasta -k l
+
+	Help Programm
+	-------------
+
+	optional arguments:
+		- \-h, --help
+						show this help message and exit
+		- \-v, --version
+						display concatFastasFile.py version number and exit
+
+	Input mandatory infos for running:
+		- \-f <filename>, --fasta <filename>
+						fasta files
+		- \-l <int>, --len <int>
+						lensize cutoff
+		- \-o <filename>, --out <filename>
+						Name of output file
+		- \-k <g/greater/l/lower>, --keep <g/greater/l/lower>
+						choice keep sequences size greater than -l (g/greater) or keep lower (l/lower)
+
+"""
+
 
 ##################################################
 ## Modules
@@ -34,16 +77,16 @@ if __name__ == "__main__":
 	# Initializations
 	start_time = strftime("%d-%m-%Y_%H:%M:%S", localtime())
 	# Parameters recovery
-	parser = argparse.ArgumentParser(prog='extractSeqFastaFromLen.py', description='''This Programme extract Fasta Seq with liste keep''')
+	parser = argparse.ArgumentParser(prog='extractSeqFastaFromLen.py', description='''This Programme filter sequences by length''')
 	parser.add_argument('-v', '--version', action='version', version='You are using %(prog)s version: ' + version, help=\
 						'display extractSeqFastaFromLen.py version number and exit')
 	#parser.add_argument('-dd', '--debug',choices=("False","True"), dest='debug', help='enter verbose/debug mode', default = "False")
 
-	files = parser.add_argument_group('Input info for running')
-	files.add_argument('-f', '--fasta', metavar="<filename>",type=existant_file, required=True, dest = 'fastaFile', help = 'fasta files')
-	files.add_argument('-l', '--len', metavar="<int>", required=True, type = int, dest = 'lenSize', help = 'lensize cutoff')
-	files.add_argument('-o', '--out', metavar="<filename>", required=True, dest = 'paramoutfile', help = 'Name of output file')
-	files.add_argument('-k', '--keep', metavar="<g/greater/l/lower>", required=True, dest = 'keepValue',choices = ["g","greater","l","lower"], help = 'choice keep sequences size greater than -l (g/greater) or keep lower (l/lower)')
+	filesreq = parser.add_argument_group('Input mandatory infos for running')
+	filesreq.add_argument('-f', '--fasta', metavar="<filename>",type=existant_file, required=True, dest = 'fastaFile', help = 'fasta files')
+	filesreq.add_argument('-l', '--len', metavar="<int>", required=True, type = int, dest = 'lenSize', help = 'lensize cutoff')
+	filesreq.add_argument('-o', '--out', metavar="<filename>", required=True, dest = 'paramoutfile', help = 'Name of output file')
+	filesreq.add_argument('-k', '--keep', metavar="<g/greater/l/lower>", required=True, dest = 'keepValue',choices = ["g","greater","l","lower"], help = 'choice keep sequences size greater than -l (g/greater) or keep lower (l/lower)')
 
 	# Check parameters
 	args = parser.parse_args()
