e1f96da58c301e9b1a698b03f30f1796bc119098
sravel <sebastien.ravel@cirad.fr>
2016-08-25 10:51:50 +0200

    merge V2 + script

--- local/grepMotifFromAlignment.py
+++ local/grepMotifFromAlignment.py
@@ -100,14 +100,14 @@ if __name__ == "__main__":
 	outputfilename = relativeToAbsolutePath(args.paramoutfile)
 
 	if args.listKeepFile not in ["ALL"]:
-		listKeepFile = loadInList(existant_file(args.listKeepFile))
+		listKeepSouche = loadInList(existant_file(args.listKeepFile))
 	else:
-		listKeepFile = "ALL"
+		listKeepSouche = "ALL"
 
 
 	print("\t - Input pathDirectory is: %s" % pathDirectory)
 	print("\t - Output file name is: %s" % outputfilename)
-	print("\t - You want to keep strain:\n%s" % "\n".join(listKeepFile))
+	print("\t - You want to keep strain:\n%s" % "\n".join(listKeepSouche))
 
 
 	dicoOutputTxt = {}
@@ -116,28 +116,45 @@ if __name__ == "__main__":
 	dicoFileCountSNP ={}
 	fileEmpty = 0
 	listFileEmpty = []
+	ctr = 1
+	nbMotifTotal=0
 
 	for filein in pathDirectory.listFiles:
-		print(filein)
+		ctr += 1
+		if ((ctr % 100 == 0) and (ctr != 0)) or (float(ctr) == len(pathDirectory.listFiles)):
+			percent = (float(ctr)/float(len(pathDirectory.listFiles)))*100
+			sys.stdout.write("\rProcessed up to %0.2f %%..." % percent)
+			sys.stdout.flush()
+
+		#print(filein)
 		dicoSeqSNP = {}
 		nbSNP = 0
 		tableauSoucheName = []
 		# lecture de l'alignement
 		#alignment = AlignIO.read(open(resultataligment,"r"), "fasta")
-		#try:
-		alignment = AlignIO.read(open(filein,"r"), "fasta")
-		#print(alignment)
+
+		alignmentStart = AlignIO.read(open(filein,"r"), "fasta")
+		# cree un nouvelle alignement avec que les souches voulus:
+		keepListRecord = []
+
+		for record in alignmentStart:
+			if record.id not in listKeepSouche and "ALL" in listKeepSouche:
+				listKeepSouche.append(record.id)
+			#print(record.id)
+			if record.id in listKeepSouche:
+				keepListRecord.append(record)
+				tableauSoucheName.append(record.id)
+				if record.id not in dicoSeqSNP.keys():
+					dicoSeqSNP[record.id] = ""
+		alignment = MultipleSeqAlignment(keepListRecord)
 		lenAlignement = int(alignment.get_alignment_length())
-		for record in alignment:
-			tableauSoucheName.append(record.id)
-			if record.id not in dicoSeqSNP.keys():
-				dicoSeqSNP[record.id] = ""
-			#else:
-				#print("Exists")
+		#print(alignment)
 		#print(tableauSoucheName)
+		#print(len(tableauSoucheName))
 
 		for indice in range(0,lenAlignement):
 			tab = list(alignment[:,indice])
+			#print(tab)
 			nbO = tab.count(tab[0])
 			nbA = tab.count("A")
 			nbC = tab.count("C")
@@ -147,109 +164,91 @@ if __name__ == "__main__":
 			nbGap = tab.count("-")
 			sommeACTG = nbA + nbC + nbT + nbG
 			allcount = sommeACTG + nbN + nbGap
-			if int(allcount) != len(tableauSoucheName):
+			if int(allcount) != len(alignment):						# test si total = nombre de souche
 				print( sommeACTG, nbA , nbC , nbT, nbG,nbN, nbGap)
 				print( tab)
 				exit()
 			if nbGap == 0 :
-				if nbO != sommeACTG and nbN == 0 :
+				if nbO != sommeACTG and nbN == 0:
 					nbSNP+=1
+					#print(indice)
 					for lentabi in range(0,len(tab)):
 						dicoSeqSNP[tableauSoucheName[lentabi]] += (tab[lentabi])
 
-		nbSNPtotal=len(dicoSeqSNP[tableauSoucheName[lentabi]])
+		nbSNPtotal=nbSNP
 		if nbSNPtotal == 0:
 			fileEmpty += 1
 			listFileEmpty.append(filein)
 
+		else:
+			nbMotifTotal+=1
+			listMotif = []
+			for geneId, sequence in dicoSeqSNP.items():
+				nbSNPtotal = (len(sequence))
+
+				listMotif.append(sequence)
+
+			nameMGG = filein.split("/")[-1].replace("_Orthologue_macse_NT.fasta","")
+			if nameMGG not in dicoFileCountSNP.keys():
+				dicoFileCountSNP[nameMGG] = {"NBSNP":nbSNPtotal,
+											 "lenAlign":lenAlignement}
 
 
-					#print(lentabi)
-				#dicoSeqSNP[]
-			#print(tab)
-				#print(nbO, sommeACTG)
-		#print("il y a %i SNP" %nbSNP)
-		#print(dict2fasta(dicoSeqSNP))
-		#except:
-			##print("Unexpected error:", sys.exc_info()[0])
-			#fileEmpty += 1
-			#listFileEmpty.append(filein)
-			#print("ERREUR dans le fichier %s , pas d'alignement lisible (possible filtre 0 gap supprime toutes les positions)" % filein)
-			#exit()
-
-
-
-
-
-		listMotif = []
-
-		for geneId, sequence in dicoSeqSNP.items():
-			nbSNPtotal = (len(sequence))
-			listMotif.append(sequence)
-
-		nameMGG = filein.split("/")[-1].replace("_Orthologue_macse_NT.fasta","")
-		if nameMGG not in dicoFileCountSNP.keys():
-			dicoFileCountSNP[nameMGG] = {"NBSNP":nbSNPtotal,
-										 "lenAlign":lenAlignement}
-
-
-		dicoFilenbSNP[nbSNPtotal] = (dicoFilenbSNP.get(nbSNPtotal, 1))+1
-
-
-		#print(nbSNPtotal)
-
-		dicoCompteMotif = {k: listMotif.count(k) for k in set(listMotif)}
-		#print(dict2txt(dicoCompteMotif))
-
-		#test si 1 SNP et partager pas combien de souche:
-		print("il y a %i SNP\n"% len(list(dicoCompteMotif)[0]))
-		nbSNP = len(list(dicoCompteMotif)[0])
-
-		if nbSNP == 2:
-			values = list(dicoCompteMotif.values())
-			if 1 in values:
-				print("the file %s containe SNP Uninformatif\n" % filein)
-
-		dicoTranslateMotif2Code = {}
-		code = 10
-		for motifUniq in dicoCompteMotif.keys():
-			dicoTranslateMotif2Code[motifUniq] = code
-			code+=1
-
-
-		for geneId, sequence in dicoSeqSNP.items():
-			codeSeq = dicoTranslateMotif2Code[sequence]
-			if geneId not in dicoOutputTxt.keys():
-				dicoOutputTxt[geneId] = [str(codeSeq)]
+			if nbSNPtotal not in dicoFilenbSNP.keys():
+				dicoFilenbSNP[nbSNPtotal] = 1
 			else:
-				dicoOutputTxt[geneId].append(str(codeSeq))
+				dicoFilenbSNP[nbSNPtotal] +=1
+
+			#print(nbSNPtotal)
+
+			dicoCompteMotif = {k: listMotif.count(k) for k in set(listMotif)}
+			#print(dict2txt(dicoCompteMotif))
+
+			dicoTranslateMotif2Code = {}
+			code = 10
+			for motifUniq in dicoCompteMotif.keys():
+				dicoTranslateMotif2Code[motifUniq] = code
+				code+=1
 
 
+			for geneId, sequence in dicoSeqSNP.items():
+				codeSeq = dicoTranslateMotif2Code[sequence]
+				if geneId not in dicoOutputTxt.keys():
+					dicoOutputTxt[geneId] = [str(codeSeq)]
+				else:
+					dicoOutputTxt[geneId].append(str(codeSeq))
 
+	output_handle = open(outputfilename, "w")
+	#print(dictList2txt(dicoOutputTxt))
+	outputTxt = ""
+	#for key in sorted(dicoOutputTxt.keys()):
+	for key in listKeepSouche:
+		value = "\t".join(dicoOutputTxt[key])
+		outputTxt += "%s\t%s\n" % (str(key),str(value))
+	output_handle.write(outputTxt)
 
-
-
-	with open(outputfilename,"w") as output_handle:
-		output_handle.write(dictList2txt(dicoOutputTxt))
-
-	outputListEmpty = open("outputListEmpty.txt", "w")
+	outputListEmpty = open(basename+"_outputListEmpty.txt", "w")
 	for fileEmptyName in listFileEmpty:
 		outputListEmpty.write(fileEmptyName+"\n")
-	outputListEmpty.close()
 
-	with open("NBSNP-file.log","w") as output_handle:
-		output_handle.write(dictDict2txt(dicoFileCountSNP))
 
-	with open("NBSNP-file-count.log","w") as output_handle:
-		output_handle.write(dict2txt(dicoFilenbSNP))
+	with open(basename+"_LenAlign_nbSNP.txt","w") as output1:
+		txt1 = dictDict2txt(dicoFileCountSNP)
+		output1.write(txt1)
+
+
+	with open(basename+"_nbSNPallFile.txt","w") as output2:
+		txt1 = dict2txt(dicoFilenbSNP)
+		output2.write(txt1)
+
 
 	print("\n\nExecution summary:")
 
 	print("  - Outputting \n\
-	Il y a au final %i SNP dans tout les MGG\n\
+	Il y a au final %i Motif dans tout les MGG\n\
 	Il y a %i fichiers vides\n\
 	les sequences sont ajouter dans le fichier %s\n\
-	la liste des fichiers vides est dans le fichier outputListEmpty.txt" %(nbSNPtotal,fileEmpty,outputfilename))
+	la liste des fichiers vides est dans le fichier outputListEmpty.txt" %(nbMotifTotal,fileEmpty,outputfilename))
 	print("\nStop time: ", strftime("%d-%m-%Y_%H:%M:%S", localtime()))
 	print("#################################################################")
 	print("#                        End of execution                       #")
