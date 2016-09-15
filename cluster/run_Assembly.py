f83839510cf27461b57dfe2821c187b3eb64c6c4
sravel <sebastien.ravel@cirad.fr>
2016-09-15 11:18:17 +0200

    update Cluster scripts

--- cluster/run_Assembly.py
+++ cluster/run_Assembly.py
@@ -20,7 +20,7 @@
 	Example
 	-------
 
-	>>> run_Assembly.py -d fastq/ -o assemblyFinal
+	>>> run_Assembly.py -f fastq/ -o assemblyFinal
 
 	Help Programm
 	-------------
@@ -32,7 +32,7 @@
 						display run_Assembly version number and exit
 
 	Input mandatory infos for running:
-		- \-d <path/to/directory>, --directory <path/to/directory>
+		- \-f <path/to/directory>, --directory <path/to/directory>
 						path with Fastq files
 		- \-o <path/to/directory>, --out <path/to/directory>
 						Output Path
@@ -67,13 +67,13 @@ if __name__ == "__main__":
 	# Initializations
 	start_time = strftime("%d-%m-%Y_%H:%M:%S", localtime())
 	# Parameters recovery
-	parser = argparse.ArgumentParser(prog='run_Assembly.py', description='''This Programme run assembly of Jerome Gouzi pipeline for fastq file''')
+	parser = argparse.ArgumentParser(prog='run_Assembly.py', description='''This Programme run assembly of Jerome Gouzi pipeline for fastq file, fastq MUST be ?z format''')
 	parser.add_argument('-v', '--version', action='version', version='You are using %(prog)s version: ' + version, help=\
 						'display run_Assembly version number and exit')
 	#parser.add_argument('-dd', '--debug',choices=("False","True"), dest='debug', help='enter verbose/debug mode', default = "False")
 
 	filesreq = parser.add_argument_group('Input mandatory infos for running')
-	filesreq.add_argument('-d', '--directory', metavar="<path/to/directory>", type = directory, required=True, dest = 'dirPath', help = 'path With Fastq files')
+	filesreq.add_argument('-f', '--fastq', metavar="<path/to/directory>", type = directory, required=True, dest = 'dirPath', help = 'path With Fastq files')
 	filesreq.add_argument('-o', '--out', metavar="<path/to/directory>", type = directory, required=True, dest = 'outPath', help = 'Output Path')
 
 	args = parser.parse_args()
@@ -85,23 +85,64 @@ if __name__ == "__main__":
 	print('Start time: ', start_time,'\n')
 
 	# Récupère le fichier de conf passer en argument
-	workingObjDir = args.dirPath
-	outObjDir = args.outPath
+	pathFastqFile = args.dirPath
+	pathFileOut = args.outPath
 
-	print("Workink Directory: %s" % workingObjDir.pathDirectory)
-
-	# création d'un output assembly dans le out et trash
-	try:
-		os.mkdir(outObjDir.pathDirectory+"/trash")
-	except FileExistsError:
-		pass
-	try:
-		os.mkdir(outObjDir.pathDirectory+"/sh")
-	except FileExistsError:
-		pass
+	outputSHDir = pathFileOut.pathDirectory+"sh/"
+	outputTrashDir = pathFileOut.pathDirectory+"trash/"
+	SGENameFile = outputSHDir+"submitQsubBLAST.sge"
 
 
-	nbJob = str(int(len(workingObjDir.listFiles)/2))
+	# resume value to user
+	print(" - Intput Info:")
+	print("\t - Working in directory: %s" % pathFileOut.pathDirectory)
+	print("\t - Fastq were in directory: %s" % pathFastqFile.pathDirectory)
+
+	print(" - Output Info:")
+	print("\t - Output were in directory: %s" % pathFileOut.pathDirectory)
+	print("\t - Output sh were in directory: %s" % outputSHDir)
+	print("\t - Output trash were in directory: %s\n\n" % outputTrashDir)
+
+
+	# build directory out
+	os.makedirs(outputSHDir, exist_ok=True)															# création d'un dossier sh_scripts pour lancer les analyses structures
+	os.makedirs(outputTrashDir, exist_ok=True)
+
+	nbJob = str(int(len(pathFastqFile.listFiles)/2))
+
+	# Parcours des fichiers
+	count=1
+	listFiles = []
+
+	for fileIn in pathFastqFile.listFiles:
+		txt = ""
+		fileName = fileIn.split("/")[-1]
+		extention = fileName.split(".")[-1]
+		baseDir = "/".join(fileIn.split("/")[:-1])
+		#basename = fileIn.split("/")[-1].split(".")[0].split("_")[0]
+		basename = fileIn.split("/")[-1].split(".")[0]
+		rValue = "_"+fileIn.split("/")[-1].split(".")[0].split("_")[-1]
+		extention = "."+".".join(fileIn.split("/")[-1].split(".")[1:])
+		#print(basename+rValue+extention)
+
+		if "z" not in extention:
+			print("Warning, file %s must be fastq.?z format" % fileIn)
+			exit()
+		else:
+
+			os.mkdir(pathFileOut.pathDirectory+basename, exist_ok=True)
+
+			cmd = "ln -s "+pathFastqFile.pathDirectory+fileName+" "+pathFileOut.pathDirectory+basename+"/"+fileName
+			os.system(cmd)
+
+			if basename not in listFiles:
+				listFiles.append(basename)
+
+				#txt += """gzip """+baseDir+"""/*\n"""
+				txt += """/NAS/BAILLARGUET/BGPI/tools/lipm_assembly/bin/lipm_assemble_solexa_pe.pl --datadir """+pathFileOut.pathDirectory+basename+""" --outdir """+pathFileOut.pathDirectory+basename+""" --outprefix """+basename+""" --log """+pathFileOut.pathDirectory+basename+"""/log.txt\n"""
+				with open(outputSHDir+"/"+str(count)+"-assembly.sh","w") as shScript:
+					shScript.write(txt)
+				count+=1
 
 	# création d'un script de qsub array
 	headerJoB = """
@@ -110,49 +151,17 @@ if __name__ == "__main__":
 #$ -N ASSEMBLY
 #$ -cwd
 #$ -V
-#$ -e """+outObjDir.pathDirectory+"/trash"+"""
-#$ -o """+outObjDir.pathDirectory+"/trash"+"""
+#$ -e """+outputTrashDir+"""
+#$ -o """+outputTrashDir+"""
 #$ -q long.q
 #$ -t 1-"""+nbJob+"""
 #$ -tc 10
 #$ -S /bin/bash
 
-qsub -N denovo -q long.q -e trash -o trash -l mem_free=100G -cwd """+outObjDir.pathDirectory+"sh"+"""/$SGE_TASK_ID-assembly.sh
+qsub -N denovo -q long.q -e trash -o trash -l mem_free=100G -cwd """+outputSHDir+"""/$SGE_TASK_ID-assembly.sh
 	"""
-	SGEFile = open(outObjDir.pathDirectory+"submitQsub.sge","w")
-	SGEFile.write(headerJoB)
-	#print(headerJoB)
-
-	# Parcours des fichiers
-	count=1
-	listFiles = []
-
-	for fileIn in workingObjDir.listFiles:
-		txt = ""
-		fileName = fileIn.split("/")[-1]
-		baseDir = "/".join(fileIn.split("/")[:-1])
-		#basename = fileIn.split("/")[-1].split(".")[0].split("_")[0]
-		basename = fileIn.split("/")[-1].split(".")[0]
-		rValue = "_"+fileIn.split("/")[-1].split(".")[0].split("_")[-1]
-		extention = "."+".".join(fileIn.split("/")[-1].split(".")[1:])
-
-		#print(basename+rValue+extention)
-		try:
-			os.mkdir(outObjDir.pathDirectory+basename)
-		except FileExistsError:
-			pass
-		cmd = "ln -s "+workingObjDir.pathDirectory+fileName+" "+outObjDir.pathDirectory+basename+"/"+fileName
-		os.system(cmd)
-
-		if basename not in listFiles:
-			listFiles.append(basename)
-
-			#txt += """gzip """+baseDir+"""/*\n"""
-			txt += """/NAS/BAILLARGUET/BGPI/tools/lipm_assembly/bin/lipm_assemble_solexa_pe.pl --datadir """+outObjDir.pathDirectory+basename+""" --outdir """+outObjDir.pathDirectory+basename+""" --outprefix """+basename+""" --log """+outObjDir.pathDirectory+basename+"""/log.txt --mink 50 --minlen 200\n"""
-			with open(outObjDir.pathDirectory+"sh"+"/"+str(count)+"-assembly.sh","w") as shScript:
-				shScript.write(txt)
-			count+=1
-	SGEFile.close()
+	with open(pathFileOut.pathDirectory+"submitQsub.sge","w") as SGEFile:
+		SGEFile.write(headerJoB)
 
 	print("\n  - Outputting \n\
 \t- %i directories have been created base on the file name, contain link to fastq and output of assembly\n\
