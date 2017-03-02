0e949f39b1be81be2712914a85ce65042b6956f6
sravel <sebastien.ravel@cirad.fr>
2017-03-02 13:54:33 +0100

    add output gzip file

--- local/vcfToTab.py
+++ local/vcfToTab.py
@@ -42,6 +42,9 @@
 						RatioAD threshold (default = 0.9)
 		- \-o <path/to/outputDir>, --out <path/to/outputDir>
 						Name of output directory
+		- \-c, --compress
+						gzip output file
+
 
 """
 
@@ -57,6 +60,7 @@ from MODULES_SEB import dict2txt, loadInList, dictDict2txt, printCol, relativeTo
 ## Python modules
 import argparse
 from time import localtime, strftime
+import gzip
 
 try:
 	import egglib3 as egglib
@@ -93,6 +97,7 @@ if __name__ == "__main__":
 	files.add_argument('-d', '--depth', metavar="<int>",type=int, default=10, dest = 'thresholdDepth', help = 'Depth threshold (default = 10)')
 	files.add_argument('-r', '--ratioAD', metavar="<int>",type=float, default=0.9, dest = 'thresholdRatio', help = 'RatioAD threshold (default = 0.9)')
 	files.add_argument('-o', '--out', metavar="<path/to/outputDir>",type = directory, default="./", dest = 'pathOut', help = 'Name of output directory')
+	files.add_argument('-c', '--compress',action ='store_true', dest = 'compress', help = 'gzip output file')
 
 
 	# Check parameters
@@ -106,6 +111,7 @@ if __name__ == "__main__":
 
 	pathFiles = args.filesDir
 	pathFilesOut = args.pathOut
+	compress = args.compress
 	thresholdDepth = args.thresholdDepth
 	thresholdRatio = args.thresholdRatio
 
@@ -118,9 +124,11 @@ if __name__ == "__main__":
 	print("\t - RatioAD threshold : %s" % thresholdRatio)
 
 	print(" - Output Info:")
+	if compress:
+		print("\t - Output tab will be gzip")
 	print("\t - Output tab created into directory:  %s\n\n" % pathFilesOut.pathDirectory)
 
-
+	exit()
 
 	for vcfFile in pathFiles.lsExtInDirToList(["vcf","gz"]):
 		# récupère le nom du fichier vcf In
@@ -156,108 +164,110 @@ if __name__ == "__main__":
 		print 'number of samples: %s\n' % vcf.num_samples
 		printCol.green('samples: %s' % "\t".join(samples))
 
-		with open(outFileName,"w") as outputFile:
-			txtout = 'Chromosome\tPosition\tReference\t%s\n' % "\t".join(samples)
-			outputFile.write(txtout)
+
+		if compress:
+			if ".gz" not in outFileName:
+				outputFile = gzip.open(outFileName+'.gz', 'wb')
+		else:
+			outputFile =  open(outFileName,"w")
+		txtout = 'Chromosome\tPosition\tReference\t%s\n' % "\t".join(samples)
+		outputFile.write(txtout)
 
 
-			for line in vcfFileOpen:
-				if extention == "gz":							# Parcours des positions du vcf si compresser
-					chrom, pos, nall = vcf.read_line(line)
-				else: 											# Parcours des positions du vcf si décompresser
-					chrom, pos, nall = line
+		for line in vcfFileOpen:
+			if extention == "gz":							# Parcours des positions du vcf si compresser
+				chrom, pos, nall = vcf.read_line(line)
+			else: 											# Parcours des positions du vcf si décompresser
+				chrom, pos, nall = line
 
-				pos+=1											# egglib commence la position à 0 donc on ajoute 1
+			pos+=1											# egglib commence la position à 0 donc on ajoute 1
 
-				variant = vcf.last_variant()					# récupère la ligne du vcf
+			variant = vcf.last_variant()					# récupère la ligne du vcf
 
-				#variant.alleles retourne l'allele de référence en premier puis les autres allèles
-				#print "\n\n", nall, variant.alleles
-				alt, alt2, alt3 = "", "", ""					# initialise les variants à vide
-				if nall == 1:
-					ref = variant.alleles[0]
-				elif nall == 2:
-					ref, alt = variant.alleles					# récupère les allèles dans un tuple
-				elif nall == 3:
-					ref, alt, alt2 = variant.alleles
-				elif nall == 4:
-					ref, alt, alt2, alt3 = variant.alleles
+			#variant.alleles retourne l'allele de référence en premier puis les autres allèles
+			#print "\n\n", nall, variant.alleles
+			alt, alt2, alt3 = "", "", ""					# initialise les variants à vide
+			if nall == 1:
+				ref = variant.alleles[0]
+			elif nall == 2:
+				ref, alt = variant.alleles					# récupère les allèles dans un tuple
+			elif nall == 3:
+				ref, alt, alt2 = variant.alleles
+			elif nall == 4:
+				ref, alt, alt2, alt3 = variant.alleles
 
-				if len(ref) < 2 and len(alt) < 2 and len(alt2) < 2 and len(alt3) < 2:		# test si la présence d'un indel
+			if len(ref) < 2 and len(alt) < 2 and len(alt2) < 2 and len(alt3) < 2:		# test si la présence d'un indel
 
-					try:
-						listDepth = []
-						listADratio = []
-						i = 0
-						for dico in variant.samples: 							# variant.samples retourne une liste de dictionnaire / individu (dictionnaire avec 'GT' minimum)
-							# On récupère les profondeurs et ratio AD
-							if "DP" in dico and dico['DP'] != ():				# dico["DP"] est vide si certains individus n'ont pas de mapping (. dans le vcf)
-								#print dico['DP'][0]
-								listDepth.append(dico['DP'][0])
-							else:
-								listDepth.append(0)
-							if "AD" in dico and dico["GT"] != ('0',) and dico['AD'] != ():		# test aussi si le génotype n'est pas la reference et si dico["AD"] est vide si certains individus n'ont pas de mapping (. dans le vcf)
-								numVariant = dico["GT"][0]										# si plusieurs variants récupére quel est celui de l'individu courent
-								nbReadsRef, nbReadsAlt = dico['AD'][0], dico['AD'][int(numVariant)]
-								#if pos in [3269,6773, 31671]: printCol.yellow(samples[i]+"  nbReadsRef "+str(nbReadsRef)+"  nbReadsAlt "+str(nbReadsAlt))
-								if nbReadsAlt != 0:
-									ADratio = nbReadsRef/nbReadsAlt
-									listADratio.append(ADratio)
-								else:										# si nbReadsAlt = 0 on ajoute 0 pour la valeur du ratio AD
-									listADratio.append(0)
-							else:											# si pas de Variant pour un individu on ajoute None au ratio AD
-								listADratio.append("None")
-							i+=1
+				try:
+					listDepth = []
+					listADratio = []
+					i = 0
+					for dico in variant.samples: 							# variant.samples retourne une liste de dictionnaire / individu (dictionnaire avec 'GT' minimum)
+						# On récupère les profondeurs et ratio AD
+						if "DP" in dico and dico['DP'] != ():				# dico["DP"] est vide si certains individus n'ont pas de mapping (. dans le vcf)
+							#print dico['DP'][0]
+							listDepth.append(dico['DP'][0])
+						else:
+							listDepth.append(0)
+						if "AD" in dico and dico["GT"] != ('0',) and dico['AD'] != ():		# test aussi si le génotype n'est pas la reference et si dico["AD"] est vide si certains individus n'ont pas de mapping (. dans le vcf)
+							numVariant = dico["GT"][0]										# si plusieurs variants récupére quel est celui de l'individu courent
+							nbReadsRef, nbReadsAlt = dico['AD'][0], dico['AD'][int(numVariant)]
+							#if pos in [3269,6773, 31671]: printCol.yellow(samples[i]+"  nbReadsRef "+str(nbReadsRef)+"  nbReadsAlt "+str(nbReadsAlt))
+							if nbReadsAlt != 0:
+								ADratio = nbReadsRef/nbReadsAlt
+								listADratio.append(ADratio)
+							else:										# si nbReadsAlt = 0 on ajoute 0 pour la valeur du ratio AD
+								listADratio.append(0)
+						else:											# si pas de Variant pour un individu on ajoute None au ratio AD
+							listADratio.append("None")
+						i+=1
 
-					except Exception as e:
-						print e
-						print chrom, pos, samples[i], dico
-						exit()
+				except Exception as e:
+					print e
+					print chrom, pos, samples[i], dico
+					exit()
 
 
-					# Pour récupérer la liste des génotypes de toute nos souches
-					genotypes = vcf.get_genotypes()
-					#print "genotype",genotypes.as_list()
+				# Pour récupérer la liste des génotypes de toute nos souches
+				genotypes = vcf.get_genotypes()
+				#print "genotype",genotypes.as_list()
 
-					# transforme egglib list (0,1) to ref/alt
-					variantList = [ str(vartmp[0]).replace("0",ref).replace("1",alt).replace("2",alt2).replace("3",alt3).replace("None","N") for vartmp in genotypes.as_list()[0]]
+				# transforme egglib list (0,1) to ref/alt
+				variantList = [ str(vartmp[0]).replace("0",ref).replace("1",alt).replace("2",alt2).replace("3",alt3).replace("None","N") for vartmp in genotypes.as_list()[0]]
 
-					try:
-						# filtre sur la profondeur et le ratio AD:
-						variantFilterList = []
-						i=0
-						for variant in variantList[0:vcf.num_samples]:
-							depth = listDepth[i]
-							ratioAD = listADratio[i]
-							if int(depth) >= thresholdDepth:
-								if ratioAD != "None" and ratioAD <= thresholdRatio :
-									variantFilterList.append(variant)
-								elif ratioAD != "None" and ratioAD >= thresholdRatio:
-									variantFilterList.append("N")
-								elif ratioAD == "None" :
-									variantFilterList.append(variant)
-							else:
+				try:
+					# filtre sur la profondeur et le ratio AD:
+					variantFilterList = []
+					i=0
+					for variant in variantList[0:vcf.num_samples]:
+						depth = listDepth[i]
+						ratioAD = listADratio[i]
+						if int(depth) >= thresholdDepth:
+							if ratioAD != "None" and ratioAD <= thresholdRatio :
+								variantFilterList.append(variant)
+							elif ratioAD != "None" and ratioAD >= thresholdRatio:
 								variantFilterList.append("N")
-							i+=1
-					except Exception as e:
-						print e
-						print chrom, pos, i, dico
-						print listDepth
-						print len(listDepth)
-						print len(listADratio)
-						print len(variantFilterList)
-						exit()
+							elif ratioAD == "None" :
+								variantFilterList.append(variant)
+						else:
+							variantFilterList.append("N")
+						i+=1
+				except Exception as e:
+					print e
+					print chrom, pos, i, dico
+					print listDepth
+					print len(listDepth)
+					print len(listADratio)
+					print len(variantFilterList)
+					exit()
 
-					# Ecriture dans le fichier tabuler
-					txtout = '%s\t%s\t%s\t%s\n' %(chrom, pos, ref, "\t".join(variantFilterList))
-					outputFile.write(txtout)
+				# Ecriture dans le fichier tabuler
+				txtout = '%s\t%s\t%s\t%s\n' %(chrom, pos, ref, "\t".join(variantFilterList))
+				outputFile.write(txtout)
 
-				#else:
-					#print "INDEL at %s %s" % (chrom, pos)
-
-
-
-
+			#else:
+				#print "INDEL at %s %s" % (chrom, pos)
+	outputFile.close()
 
 	print("\n\n#################################################################")
 	print("#                        End of execution                       #")
